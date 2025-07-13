70985b8c0ad7ca718f6740b0aae3d03e397c0092
Erik-Bohlin <erik_bohlin@outlook.com>
2025-07-13 11:10:34 +0200

    Add main menu scene and UI with TextMesh Pro
    Introduced a new Menu scene with background, name input, and buttons using TextMesh Pro for UI elements. Added supporting assets including a background image, gold color preset, and TextMesh Pro resources. Updated main scene and scripts to integrate menu functionality.

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+// Sets the script to be executed later than all default scripts
+// This is helpful for UI, since other things may need to be initialized before setting the UI
+[DefaultExecutionOrder(1000)]
+public class MenuUIHandler : MonoBehaviour
+{
+    public TMP_InputField nameInputField;
+    private void Start()
+    {
+        nameInputField.onEndEdit.RemoveAllListeners();
+        nameInputField.onEndEdit.AddListener(MenuManager.Instance.ReadStringInput);
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
